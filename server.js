1ed62c22537892ec77071fb300a2e55d65815d43
keybit-1 <131838974+keybit-1@users.noreply.github.com>
2023-09-08 16:11:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/config');  // Make sure the path is correct
+const { User, BlogPost, Comment } = require('./models/relationships');  // Import models and relationships
 require('dotenv').config();
 
 const app = express();
@@ -10,9 +11,15 @@ sequelize.authenticate()
   .then(() => console.log('Database connected successfully.'))
   .catch(err => console.log('Error connecting to the database: ', err));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Sync database and then start the server
+sequelize.sync({ force: false })  // Change force to 'true' only if you want to reset your database
+  .then(() => {
+    console.log('Database synced');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  });
+
 
 
 
